2cb2da70c51e89f0a46ffa88c473c2b959135e97
Julian Callin <julian.callin@here.com>
2018-12-23 14:18:41 -0700

    Add remove duplicates and find missing number algorithms

--- data-structures/src/main/scala/com/scalaprojects/algorithms/Arrays.scala
+++ data-structures/src/main/scala/com/scalaprojects/algorithms/Arrays.scala
@@ -4,8 +4,8 @@ import scala.annotation.tailrec
 
 object Arrays {
   /** Return an array reversed by recursion
-    * @param a the array to reverse
-    * @tparam T the type of object in the array
+    * @param a array to reverse
+    * @tparam T type of object in the array
     */
   def reverseArrayRecursive[T: Manifest](a: Array[T]): Array[T] = {
     @tailrec
@@ -26,8 +26,8 @@ object Arrays {
   }
 
   /** Return an array reversed by iteration
-    * @param a the array to reverse
-    * @tparam T the type of object in the array
+    * @param a array to reverse
+    * @tparam T type of object in the array
     */
   def reverseArrayIterative[T](a: Array[T]): Array[T] = {
     // Set up bounds
@@ -46,5 +46,45 @@ object Arrays {
       a.update(j, temp)
     }
     a
+  }
+
+  /** Return an array with duplicates removed
+    * @param a array to de-duplicate
+    * @tparam T type of object in the array
+    */
+  def removeDups[T: Manifest](a: Array[T]): Array[T] = {
+    @tailrec
+    def removeDupsHelper(a: Array[T], dups: Set[T], acc: Array[T]): Array[T] = {
+      a match {
+        // If we are finished, return the accumulator
+        case Array() => acc
+        case Array(head, rest @ _*) =>
+          // Add head to the new set
+          val newDups = dups + head
+          // If we have seen the head before, don't add it to the acc array
+          if (dups.contains(head)) {
+            removeDupsHelper(rest.toArray, newDups, acc)
+          // If we haven't seen the head before, add it to the acc array
+          } else {
+            removeDupsHelper(rest.toArray, newDups, acc ++ Array(head))
+          }
+      }
+    }
+    removeDupsHelper(a, Set.empty[T], Array.empty[T])
+  }
+
+  /** Return the single missing number from an array of size n missing exactly 1 number in a sequence 1 to n
+    * @param a array to inspect
+    */
+  def findMissingNumberIn(a: Array[Int]): Int = {
+    if (a.isEmpty) {
+      0
+    } else {
+      // An array with n numbers should sum to those numbers added up
+      // The incoming array will have 1 less number
+      // If we subtract the incoming array's sum from the expected sum of a full array, we will get the missing number's value
+      val shouldSumTo: Int = (1 to (a.length + 1)).sum
+      shouldSumTo - a.sum
+    }
   }
 }
--- data-structures/src/test/scala/com/scalaprojects/algorithms/ArraysTest.scala
+++ data-structures/src/test/scala/com/scalaprojects/algorithms/ArraysTest.scala
@@ -20,22 +20,38 @@ class ArraysTest extends FunSpec with Matchers with SeveredStackTraces {
         reverseArrayRecursive(a) shouldBe Array.empty[AnyVal]
       }
     }
-  }
-  describe("when reversing an array by iteration") {
-    it("should reverse non-empty even and odd length arrays") {
-      val a = Array(1, 2, 3, 4, 5)
-      val b = Array(1, 2, 3, 4)
-      reverseArrayIterative(a) shouldBe Array(1, 2, 3, 4, 5).reverse
-      reverseArrayIterative(b) shouldBe Array(1, 2, 3, 4 ).reverse
-    }
-    it("should reverse an array with a single value") {
-      val a = Array("single")
-      reverseArrayIterative(a) shouldBe Array("single")
+    describe("when reversing an array by iteration") {
+      it("should reverse non-empty even and odd length arrays") {
+        val a = Array(1, 2, 3, 4, 5)
+        val b = Array(1, 2, 3, 4)
+        reverseArrayIterative(a) shouldBe Array(1, 2, 3, 4, 5).reverse
+        reverseArrayIterative(b) shouldBe Array(1, 2, 3, 4).reverse
+      }
+      it("should reverse an array with a single value") {
+        val a = Array("single")
+        reverseArrayIterative(a) shouldBe Array("single")
 
+      }
+      it("should reverse an empty array") {
+        val a = Array.empty[AnyVal]
+        reverseArrayIterative(a) shouldBe Array.empty[AnyVal]
+      }
     }
-    it("should reverse an empty array") {
-      val a = Array.empty[AnyVal]
-      reverseArrayIterative(a) shouldBe Array.empty[AnyVal]
+    describe("when removing duplicates from an array") {
+      it("should remove duplicates from a non-empty array") {
+        val a = Array(1, 2, 2, 3, 4, 5, 4, 1)
+        removeDups(a) shouldBe Array(1, 2, 3, 4, 5)
+      }
+      it("should remove duplicates from an empty array") {
+        val a = Array.empty[AnyVal]
+        removeDups(a) shouldBe Array.empty[AnyVal]
+      }
+    }
+    describe("when finding the missing number in a sequential array of integers") {
+      it("should find the missing number in an array of size 9") {
+        val a: Array[Int] = Array(1, 2, 3, 5, 6, 7, 8, 9, 10)
+        findMissingNumberIn(a) shouldBe 4
+      }
     }
   }
 }
