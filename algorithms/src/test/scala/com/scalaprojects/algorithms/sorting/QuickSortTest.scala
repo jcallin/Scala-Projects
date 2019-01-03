8c1b18805a3593b69d3b980b3627c50ad111593a
Julian Callin <julian.callin@here.com>
2019-01-03 14:54:00 -0800

    Update quick sort test

--- algorithms/src/test/scala/com/scalaprojects/algorithms/sorting/QuickSortTest.scala
+++ algorithms/src/test/scala/com/scalaprojects/algorithms/sorting/QuickSortTest.scala
@@ -6,8 +6,8 @@ class QuickSortTest extends FunSpec with Matchers with SeveredStackTraces {
 
   import com.scalaprojects.algorithms.sorting.QuickSort._
 
-  describe("Sorting algorithms") {
-    it("should sort an array using quicksort") {
+  describe("quick sort traditional") {
+    it("should sort an array using traditional quicksort") {
       val a = Array(2, 9, 5, 2, 8, 3, 5, 12, 7, 1, 0, 9, 1)
       quickSort(a) shouldBe a.sorted
       val b = Array(2, 7, 2, 4, 3)
