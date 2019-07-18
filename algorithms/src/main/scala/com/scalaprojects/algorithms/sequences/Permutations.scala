0dd9cb92166c1b1fac2020ec1458bc342fe2361e
Julian Callin <j.callin@hotmail.com>
2019-07-18 10:15:33 -0700

    Permutations of a sequence (#11)
    * Add permutations and test
    
    * Add edge cases
    
    * Add pretty typing

---
+++ algorithms/src/main/scala/com/scalaprojects/algorithms/sequences/Permutations.scala
@@ -0,0 +1,28 @@
+package com.scalaprojects.algorithms.sequences
+
+object Permutations {
+  type Permutation[T] = List[T]
+  type Permutations[T] = List[Permutation[T]]
+  /**
+   * Return all permutations of a given sequence
+   *
+   */
+  def allPermutationsOf[T](sequence: List[T]): Permutations[T] = {
+    def allPermutationsOfHelper(seq: List[T], acc: Permutations[T]): Permutations[T] = {
+      seq match {
+        case Nil => acc
+        case head :: Nil => List(head) :: acc
+        case head :: tail =>
+          allPermutationsOfHelper(tail, acc).flatMap {
+            // Use a permutation and insert head at all places
+            perm =>
+              for (i <- 0 until (perm.length + 1)) yield {
+                val (front, back) = perm.splitAt(i)
+                front ::: List(head) ::: back
+              }
+          }
+      }
+    }
+    allPermutationsOfHelper(sequence, List.empty[Permutation[T]])
+  }
+}
---
+++ algorithms/src/test/scala/com/scalaprojects/algorithms/sequences/PermutationsTest.scala
@@ -0,0 +1,23 @@
+package com.scalaprojects.algorithms.sequences
+
+import com.scalaprojects.tools.testtools.ScalaProjectsSpec
+
+class PermutationsTest extends ScalaProjectsSpec {
+  import Permutations._
+  describe("Permutations finder") {
+    it("should find all permutations for a given list") {
+      allPermutationsOf(List(1, 2, 3)) shouldBe List(
+        List(1, 2, 3),
+        List(2, 1, 3),
+        List(2, 3, 1),
+        List(1, 3, 2),
+        List(3, 1, 2),
+        List(3, 2, 1))
+    }
+    it("should cover empty list and single item list") {
+      allPermutationsOf(List.empty[Int]) shouldBe List.empty[List[Int]]
+      allPermutationsOf(List(0)) shouldBe List(List(0))
+    }
+  }
+
+}
