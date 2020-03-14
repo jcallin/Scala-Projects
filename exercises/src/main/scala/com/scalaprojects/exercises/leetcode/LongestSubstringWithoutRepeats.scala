9c48d2626c188bd7b1ac74b453f14b494b04cf56
Julian Callin <j.callin@hotmail.com>
2020-03-14 15:46:53 -0700

    Add Longest repeating substring problem and solution, updates readmes

---
+++ exercises/src/main/scala/com/scalaprojects/exercises/leetcode/LongestSubstringWithoutRepeats.scala
@@ -0,0 +1,34 @@
+package com.scalaprojects.exercises.leetcode
+
+import scala.collection.mutable
+
+object LongestSubstringWithoutRepeats {
+  def longestSubstringWithoutRepeats(s: String): Int = {
+    // Track each letter seen, as well as the index it was last seen at
+    var longestSeen = 0
+    val lettersSeenInSubstring = mutable.Map[Char, Int]()
+    var tracker = 0
+
+    while (tracker < s.length) {
+      val currentLetter = s(tracker)
+
+      if (!lettersSeenInSubstring.contains(currentLetter)) {
+        lettersSeenInSubstring.addOne(currentLetter, tracker)
+      } else {
+        if (lettersSeenInSubstring.size > longestSeen) {
+          longestSeen = lettersSeenInSubstring.size
+        }
+
+        // Go back to the last place the letter was seen
+        tracker = lettersSeenInSubstring(currentLetter)
+        // Reset the map
+        lettersSeenInSubstring.clear()
+      }
+      tracker += 1
+    }
+    if (lettersSeenInSubstring.size > longestSeen) {
+      longestSeen = lettersSeenInSubstring.size
+    }
+    longestSeen
+  }
+}
