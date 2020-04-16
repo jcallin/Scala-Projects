26e2dd41ae8ae368a051900ce1fcb64008f4b404
Julian Callin <j.callin@hotmail.com>
2020-04-15 22:02:44 -0700

    Add leaf-similar solution and tests

--- exercises/src/main/scala/com/scalaprojects/exercises/leetcode/LeafSimilarTrees.scala
+++ exercises/src/main/scala/com/scalaprojects/exercises/leetcode/LeafSimilarTrees.scala
@@ -1,5 +1,25 @@
 package com.scalaprojects.exercises.leetcode
 
+import com.scalaprojects.exercises.leetcode.datastructures.TreeNode
+
 object LeafSimilarTrees {
-  def leafSimilar(root1: TreeNode, root2: TreeNode): Boolean = {}
+  def leafSimilar(root1: TreeNode, root2: TreeNode): Boolean = {
+
+    def getLeafArray(root: TreeNode): Array[Int] = {
+      if (isLeaf(root)) {
+        Array(root.value)
+      } else if (root.left == null) {
+        getLeafArray(root.right)
+      } else if (root.right == null) {
+        getLeafArray(root.left)
+      } else {
+        getLeafArray(root.left) ++ getLeafArray(root.right)
+      }
+    }
+
+    getLeafArray(root1) sameElements getLeafArray(root2)
+  }
+
+  def isLeaf(node: TreeNode): Boolean = node.left == null && node.right == null
+
 }
