6b7ef1c9ea885aa3a0cc46d765eeba95f433e6c5
Julian Callin <j.callin@hotmail.com>
2020-04-15 22:51:17 -0700

    Leetcode leaf similar trees (#25)
    * WIP
    
    * Add leaf-similar solution and tests
    
    * Add testAndCoverageReport commands for individual projects and root aggregate
    
    * Update travis file

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,8 +1,32 @@
+import sbt.{Command, TaskKey}
+import sbt.Keys._
+
 object Settings {
   import scoverage.ScoverageKeys._
-  val commonSettings = Seq(
+
+  // Helper to simplify command creation below
+  private implicit def taskToString(task: TaskKey[_]) = task.key.label
+
+  // Make a single command to run tests and generate coverage report
+  private def generateTestAndCoverageReportCommand(
+      coverageCommand: sbt.TaskKey[scala.Unit]
+  ): Command = {
+    Command.command(
+      "testAndCoverageReport",
+      "Turns test coverage on, tests, generates a report, and turns coverage back off",
+      detail = "Single-command method of generating a coverage report during development"
+    ) { state => "coverageOn" :: clean :: test :: coverageCommand :: "coverageOff" :: state }
+  }
+
+  val rootSettings = Seq(
     coverageEnabled := true,
     coverageMinimum := 90,
-    coverageFailOnMinimum := true
+    coverageFailOnMinimum := true,
+    commands += generateTestAndCoverageReportCommand(coverageAggregate)
   )
+
+  val commonSettings = rootSettings ++ Seq(
+    commands += generateTestAndCoverageReportCommand(coverageReport)
+  )
+
 }
