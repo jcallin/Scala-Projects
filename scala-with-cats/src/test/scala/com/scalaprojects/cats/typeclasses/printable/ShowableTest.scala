ccfd62a72e01211fcbbb1b4dd450d6e5b2e425c5
Julian Callin <j.callin@hotmail.com>
2020-04-18 14:26:18 -0700

    Add cat show methods

---
+++ scala-with-cats/src/test/scala/com/scalaprojects/cats/typeclasses/printable/ShowableTest.scala
@@ -0,0 +1,16 @@
+package com.scalaprojects.cats.typeclasses.printable
+
+import com.scalaprojects.tools.testtools.ScalaProjectsSpec
+
+import cats.implicits._
+
+class ShowableTest extends ScalaProjectsSpec {
+  it("shows a cat") {
+    import CatShowable.catShow
+
+    val cat = Cat("Felix", 10, "black")
+    val stringifiedCat = "Felix is a 10 year-old black cat"
+
+    cat.show shouldBe stringifiedCat
+  }
+}
